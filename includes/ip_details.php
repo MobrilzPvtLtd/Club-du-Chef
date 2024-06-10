851aa96f36d04f87b2a79b908643eaa68452d0f9
Shiv Singh <shivsingh7150@hotmail.com>
2024-06-10 18:06:00 +0530

    Update ip_details.php

---
+++ includes/ip_details.php
@@ -0,0 +1,27 @@
+<?php
+
+$CurrentCity = file_get_contents('https://ipapi.co/'.get_client_ip().'/city/');
+
+
+echo $CurrentCity;
+
+
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if (getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if (getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if (getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if (getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if (getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
