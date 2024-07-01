be8aba5fbf8f68d7f29cce581b98be57f9fc0bba
Shiv Singh <shivsingh7150@hotmail.com>
2024-07-01 15:44:16 +0530

    Update updateCity.php

--- admin/config/updateCity.php
+++ admin/config/updateCity.php
@@ -1,11 +1,9 @@
 <?php
 
-$CityName = [];
-
 $city = $_REQUEST['city'];
 
 $_SESSION['city'] = $city;
 
-$webpage_full_link_url = "https://" . $city . ".truewebservice.com/";
+$webpage_full_link_url = "https://" . $_SESSION['city'] . ".truewebservice.com/";
 
 echo $webpage_full_link_url;
