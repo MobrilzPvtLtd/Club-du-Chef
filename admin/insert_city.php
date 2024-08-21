4e45c790210c0c81957c36473e42bfaf077eaaca
Shiv Singh <shiv@srapsware.com>
2024-08-21 12:52:46 +0530

    refactor: Improve city slug generation in insert_city.php

--- admin/insert_city.php
+++ admin/insert_city.php
@@ -49,25 +49,7 @@ if (isset($_POST['city_submit'])) {
 //************ city Name Already Exist Check Ends ***************
 
 
-        $_FILES['city_image']['name'][$i];
-
-        if (!empty($_FILES['city_image']['name'][$i])) {
-            $file = rand(1000, 100000) . $_FILES['city_image']['name'][$i];
-            $file_loc = $_FILES['city_image']['tmp_name'][$i];
-            $file_size = $_FILES['city_image']['size'][$i];
-            $file_type = $_FILES['city_image']['type'][$i];
-            $allowed = array("image/jpeg", "image/pjpeg", "image/png", "image/gif", "image/webp", "image/svg", "application/pdf", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.openxmlformats-officedocument.wordprocessingml.template");
-            if (in_array($file_type, $allowed)) {
-                $folder = "../images/services/";
-                $new_size = $file_size / 1024;
-                $new_file_name = strtolower($file);
-                $event_image = str_replace(' ', '-', $new_file_name);
-                //move_uploaded_file($file_loc, $folder . $event_image);
-                $city_image = compressImage($event_image, $file_loc, $folder, $new_size);
-            } else {
-                $city_image = '';
-            }
-        }
+       
 
         $state_sql_1 = "SELECT * FROM  " . TBL . "states where country_id='" . $country_id . "' LIMIT 1";
         $state_rs_1 = mysqli_query($conn, $state_sql_1);
