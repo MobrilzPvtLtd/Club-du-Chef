bae46b5926b3c83f25f913f49849ea116c0eb2cd
PriyankaMobrilz <priyanka@mobrilz.com>
2024-09-10 10:44:23 +0530

    Update all-listing.php

--- all-listing.php
+++ all-listing.php
@@ -1588,20 +1588,20 @@ include "footer.php";
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="js/jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery-ui.js"></script>
-<script src="js/select-opt.js"></script>
-<script src="js/blazy.min.js"></script>
+<script src="<?php echo $slash; ?>js/jquery.min.js"></script>
+<script src="<?php echo $slash; ?>js/popper.min.js"></script>
+<script src="<?php echo $slash; ?>js/bootstrap.min.js"></script>
+<script src="<?php echo $slash; ?>js/jquery-ui.js"></script>
+<script src="<?php echo $slash; ?>js/select-opt.js"></script>
+<script src="<?php echo $slash; ?>js/blazy.min.js"></script>
 <script type="text/javascript">var webpage_full_link = '<?php echo $webpage_full_link;?>';</script>
 <script type="text/javascript">var login_url = '<?php echo $LOGIN_URL;?>';</script>
-<script src="js/custom.js"></script>
+<script src="<?php echo $slash; ?>js/custom.js"></script>
 <script src="<?php echo $slash; ?>js/slick.js"></script>
-<script src="js/listing_filter.js"></script>
-<script src="js/jquery.validate.min.js"></script>
-<script src="js/custom_validation.js"></script>
-<!--<script src="js/jquery.simplePagination.min.js"></script>-->
+<script src="<?php echo $slash; ?>js/listing_filter.js"></script>
+<script src="<?php echo $slash; ?>js/jquery.validate.min.js"></script>
+<script src="<?php echo $slash; ?>js/custom_validation.js"></script>
+<!--<script src="<?php echo $slash; ?>js/jquery.simplePagination.min.js"></script>-->
 
 <script>
     $(document).ready(function () {
