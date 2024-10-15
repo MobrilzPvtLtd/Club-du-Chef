03b424eee8bbc9dec3bd8a9519769284eb8c79e6
PriyankaMobrilz <priyanka@mobrilz.com>
2024-10-15 12:12:25 +0530

    paypal live code add for paypal payment check

--- payment_paypal_ads.php
+++ payment_paypal_ads.php
@@ -8,27 +8,23 @@ if (file_exists('config/user_authentication.php')) {
 }
 require __DIR__  . '/vendor/autoload.php';
 
+
 $user_id = $_SESSION['user_id'];
 
 if (isset($user_id)) {
-
     $user_details_row = getUser($user_id); //Fetch Logged In user data
     $user_plan = $user_details_row['user_plan']; //Fetch of Logged In user Plan
-
     $user_plan_type = getPlanType($user_plan); //Fetch Logged In User Plan details and data
 }else{
-
     header('Location: index');
     exit;
 }
 
 $footer_row = getAllFooter();
-
 $ads_enquiry_id = $_SESSION['ads_enquiry_id'];
 $ad_total_cost = $_SESSION['ad_total_cost']; 
 
 $admin_paypal_currency_code = $footer_row['admin_paypal_currency_code']; // Admin Paypal Currency Code
-
 $link_with_payment_failure = $webpage_full_link. "point_payment_paypal_failure";  //URL Payment Failure Link
 $link_with_payment_success = $webpage_full_link. "payment_paypal_success_ads" . '?' . 'ads_enquiry_id'. '=' .$ads_enquiry_id . '&amount=' . $ad_total_cost . '&currency=' . $admin_paypal_currency_code;
 
@@ -40,6 +36,12 @@ $apiContext = new \PayPal\Rest\ApiContext(
     )
 );
 
+$apiContext->setConfig([
+    'mode' => 'live',
+    'log.LogEnabled' => true,
+    'log.FileName' => 'PayPal.log',
+    'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+]);
 
 $payer = new \PayPal\Api\Payer();
 $payer->setPaymentMethod('paypal');
@@ -63,10 +65,10 @@ $payment->setIntent('sale')
 
 try {
     $payment->create($apiContext);
+
     header('Location: ' . $payment->getApprovalLink());
     exit;
     // echo $payment;
-
     // echo "\n\nRedirect user to approval_url: " . $payment->getApprovalLink() . "\n";
 }
 catch (\PayPal\Exception\PayPalConnectionException $ex) {
