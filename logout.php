0548022ec920c71c37a054dd6fe8b9e2af394ece
PriyankaMobrilz <priyanka@mobrilz.com>
2025-02-21 13:00:32 +0530

    Session wise logout now working properly

--- logout.php
+++ logout.php
@@ -1,36 +1,16 @@
 <?php
-/**
- * Created by Vignesh.
- * User: Vignesh
- */
+include "header.php";
 
-# Prevent warning. #
-error_reporting(0);
-ob_start();
-session_start();
-
-if (file_exists('config/info.php')) {
-    include('config/info.php');
-}
+include "dashboard_left_pane.php";
 
 unset($_SESSION['user_code']);
 unset($_SESSION['user_name']);
 unset($_SESSION['user_id']);
-
-
 
 if((!isset($_SESSION['user_code']) || empty($_SESSION['user_code'])) && (!isset($_SESSION['user_name']) ||  empty($_SESSION['user_name']) && (!isset($_SESSION['user_id']) ||  empty($_SESSION['user_id']))) )
 {
     header("Location: ".$webpage_full_link);
     exit();
 }
-
-//
-//session_start();
-//if(session_destroy())
-//{
-//    header("Location: index");
-//}
-
 
 ?>
